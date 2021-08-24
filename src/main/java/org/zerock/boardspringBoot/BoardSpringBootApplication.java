17005bd31f61486c19d998f996b5a8185705fbad
todojinu <nukukkoya@gmail.com>
2021-08-24 23:31:39 +0900

    First Commit

---
+++ src/main/java/org/zerock/boardspringBoot/BoardSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.boardspringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/boardspringBoot/BoardSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.boardspringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
