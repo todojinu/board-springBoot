7ccb3f7f6f67f3158ff5686361e56cfc7b28948e
todojinu <nukukkoya@gmail.com>
2021-09-01 23:16:16 +0900

    목록화면, 조회화면 JPQL 생성

--- src/main/java/org/zerock/boardspringBoot/repository/BoardRepository.java
+++ src/main/java/org/zerock/boardspringBoot/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package org.zerock.boardspringBoot.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -23,5 +25,20 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
      */
     @Query("SELECT b, r FROM Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno = :bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value = "SELECT b, w, count(r)"
+            + " FROM Board b "
+            + " LEFT JOIN b.writer w "
+            + " LEFT JOIN Reply r ON r.board = b "
+            + " GROUP BY b",
+            countQuery = "SELECT count(b) FROM Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);  // 목록화면에 필요한 데이터
+
+    @Query("SELECT b, w, count(r) "
+            + " FROM Board b "
+            + " LEFT JOIN b.writer w "
+            + " LEFT JOIN Reply r ON r.board = b "
+            + " WHERE b.bno = :bno")
+    Object getBoardByBno(@Param("bno") Long bno);
 
 }
